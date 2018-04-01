335017870688fa07b6724ae440aacd40ce807871
范洪月 <fanhongyue@zhongan.io>
2018-04-01 17:45:50 +0800

    add img

--- controllers/invitation.go
+++ controllers/invitation.go
@@ -4,8 +4,14 @@ import (
 	"panda/arithmetic"
 	"panda/models"
 
+	"bytes"
+	"encoding/base64"
+	"image/png"
+
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/orm"
+	"github.com/boombuler/barcode"
+	"github.com/boombuler/barcode/qr"
 )
 
 type InvitationController struct {
@@ -17,16 +23,17 @@ type RspInvitationCode struct {
 	InvitationUrl string `json:"invitation_url"`
 	Surplus       int    `json:"surplus"`
 	IsReward      int    `json:"isReward"`
+	Image         string `json:"image"`
 }
 
 func (c *InvitationController) HandlerGenerateInvitationCode() {
 	var (
-		err     error
-		userId  int64
-		surplus int
-		count   int
-		code    string
-		flag    int
+		err                      error
+		userId                   int64
+		surplus                  int
+		count                    int
+		code, invitationUrl, img string
+		flag                     int
 	)
 	conf := GetConfigData()
 
@@ -49,11 +56,18 @@ func (c *InvitationController) HandlerGenerateInvitationCode() {
 	} else {
 		surplus = conf.InvitationLimit - count
 	}
+
+	invitationUrl = conf.HostUrl + "/v1/tsxm/regist?code=" + code
+
+	if img, err = GenQRCode(invitationUrl); err != nil {
+		goto errDeal
+	}
 	c.Ctx.Output.JSON(RspInvitationCode{
 		Success:       true,
-		InvitationUrl: conf.HostUrl + "/v1/tsxm/regist?code=" + code,
+		InvitationUrl: invitationUrl,
 		Surplus:       surplus,
 		IsReward:      flag,
+		Image:         img,
 	}, false, false)
 	return
 errDeal:
@@ -61,5 +75,27 @@ errDeal:
 		Success: false,
 		Message: err.Error(),
 	}, false, false)
+	return
+}
+
+func GenQRCode(code string) (encode string, err error) {
+
+	var barcode barcode.Barcode
+
+	prepng := "data:image/png;base64,"
+
+	if barcode, err = qr.Encode(code, qr.L, qr.Unicode); err != nil {
+		return
+	}
+	c := new(bytes.Buffer)
+
+	if err = png.Encode(c, barcode); err != nil {
+		return
+	}
+
+	encode = base64.StdEncoding.EncodeToString(c.Bytes())
+
+	encode = prepng + encode
+
 	return
 }
